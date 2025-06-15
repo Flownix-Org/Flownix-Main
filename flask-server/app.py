c63ab4ca105d5caa22221797c9769249186b8df8
VirtualSquares <ravid0204@gmail.com>
2025-06-15 14:56:18 -0700

    Database Setup

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/register', methods = ["GET", "POST"])
+client = MongoClient("your_mongodb_uri_here")
+db = client["your_db_name"]
+
+@app.route('/register', methods=['POST'])
 def register():
-    None
+    return "Register Page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
